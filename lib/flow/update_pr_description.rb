567f3288c688650029093a363474ce60c6b73db9
Manuel Fittko <manuel.fittko@sofatutor.com>
2024-12-05 09:11:42 +0100

    add PRDescriptionUpdater to manage pull request descriptions with gem change links

---
+++ lib/flow/update_pr_description.rb
@@ -0,0 +1,38 @@
+require 'octokit'
+
+module Flow
+  class PRDescriptionUpdater
+    class << self
+      def call(gem_name, compare_url, pr_number)
+        @gem_name = gem_name
+        @compare_url = compare_url
+        @pr_number = pr_number
+        @client = Octokit::Client.new(access_token: ENV['GITHUB_TOKEN'])
+        update_description
+      end
+
+      def fetch_pr_body
+        pr = @client.pull_request(ENV['GITHUB_REPOSITORY'], @pr_number)
+        pr[:body] || ""
+      end
+
+      def update_pr_body(updated_body)
+        @client.update_pull_request(ENV['GITHUB_REPOSITORY'], @pr_number, body: updated_body)
+      end
+
+      def update_description
+        current_body = fetch_pr_body
+        link_marker = "[#{@gem_name} Changes]"
+        new_link = "#{link_marker}(#{@compare_url})"
+
+        updated_body = if current_body.include?(link_marker)
+                         current_body.gsub(/\[#{@gem_name} Changes\]\(.*\)/, new_link)
+                       else
+                         "#{new_link}\n\n#{current_body}"
+                       end
+
+        update_pr_body(updated_body)
+      end
+    end
+  end
+end
