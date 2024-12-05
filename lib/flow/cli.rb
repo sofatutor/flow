ae6a4e97a9d170a8813aa5a54da0c36c7aa01e9d
Manuel Fittko <manuel.fittko@sofatutor.com>
2024-12-05 11:00:33 +0100

    remove Thor dependency and refactor CLI to use OptionParser for command handling

--- lib/flow/cli.rb
+++ lib/flow/cli.rb
@@ -1,20 +1,40 @@
-require 'thor'
+require 'optparse'
 require_relative 'update_pr_description'
 require_relative 'check_gem_revision'
 
 module Flow
-  class CLI < Thor
-    desc "update_pr_description GEM_NAME COMPARE_URL PR_NUMBER", "Update the PR description with a link to gem changes"
-    def update_pr_description(gem_name, compare_url, pr_number)
-      Flow::PRDescriptionUpdater.call(gem_name, compare_url, pr_number)
-    end
+  class CLI
+    def self.start
+      options = {}
+      subcommand = ARGV.shift
 
-    desc "check_gem_revision GEM_NAME MAIN_BRANCH", "Check the gem revision and print the compare URL"
-    def check_gem_revision(gem_name, main_branch)
-      compare_url = GemRevisionChecker.call(gem_name, main_branch)
-      puts compare_url if compare_url
+      case subcommand
+      when 'update_pr_description'
+        OptionParser.new do |opts|
+          opts.banner = "Usage: flow update_pr_description GEM_NAME COMPARE_URL PR_NUMBER"
+          opts.on("-g", "--gem_name GEM_NAME", "Name of the gem") { |v| options[:gem_name] = v }
+          opts.on("-c", "--compare_url COMPARE_URL", "Compare URL") { |v| options[:compare_url] = v }
+          opts.on("-p", "--pr_number PR_NUMBER", "Pull Request number") { |v| options[:pr_number] = v }
+        end.parse!
+
+        Flow::PRDescriptionUpdater.call(options[:gem_name], options[:compare_url], options[:pr_number])
+
+      when 'check_gem_revision'
+        OptionParser.new do |opts|
+          opts.banner = "Usage: flow check_gem_revision GEM_NAME MAIN_BRANCH"
+          opts.on("-g", "--gem_name GEM_NAME", "Name of the gem") { |v| options[:gem_name] = v }
+          opts.on("-m", "--main_branch MAIN_BRANCH", "Main branch name") { |v| options[:main_branch] = v }
+        end.parse!
+
+        compare_url = GemRevisionChecker.call(options[:gem_name], options[:main_branch])
+        puts compare_url if compare_url
+
+      else
+        puts "Unknown subcommand: #{subcommand}"
+        puts "Available subcommands: update_pr_description, check_gem_revision"
+      end
     end
   end
 end
 
-Flow::CLI.start(ARGV) if __FILE__ == $PROGRAM_NAME
+Flow::CLI.start if __FILE__ == $PROGRAM_NAME
