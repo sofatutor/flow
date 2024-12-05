412114d8d054055c31a9a2fa280759203d10d3ff
Manuel Fittko <manuel.fittko@sofatutor.com>
2024-12-05 10:07:21 +0100

    refactor CLI commands to rename update_pr method and add check_gem_revision command

--- lib/flow/cli.rb
+++ lib/flow/cli.rb
@@ -1,11 +1,12 @@
 require 'thor'
 require 'tty-prompt'
 require_relative 'update_pr_description'
+require_relative 'check_gem_revision'
 
 module Flow
   class CLI < Thor
-    desc "update_pr GEM_NAME COMPARE_URL PR_NUMBER", "Update the PR description with a link to gem changes"
-    def update_pr(gem_name, compare_url, pr_number)
+    desc "update_pr_description GEM_NAME COMPARE_URL PR_NUMBER", "Update the PR description with a link to gem changes"
+    def update_pr_description(gem_name, compare_url, pr_number)
       prompt = TTY::Prompt.new
 
       github_token = ENV['GITHUB_TOKEN'] || (prompt.ask('Enter your GitHub token:') if prompt.stdin.tty?)
@@ -20,6 +21,11 @@ module Flow
       end
 
       Flow::PRDescriptionUpdater.call(gem_name, compare_url, pr_number)
+    end
+
+    desc "check_gem_revision GEM_NAME MAIN_BRANCH", "Check the gem revision and print the compare URL"
+    def check_gem_revision(gem_name, main_branch)
+      GemRevisionChecker.call(gem_name, main_branch)
     end
   end
 end
