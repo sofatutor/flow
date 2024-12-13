ed5ac21ab6b69ae9d3a7a67aa5ca9ee3df0d81af
Manuel Fittko <manuel.fittko@sofatutor.com>
2024-12-13 20:15:08 +0100

    refactor: streamline command execution in SystemHelper with separate methods for TTY and PTY handling

--- lib/flow/system_helper.rb
+++ lib/flow/system_helper.rb
@@ -2,26 +2,19 @@ require 'open3'
 require 'pty'
 
 module SystemHelper
-  def self.call(*command_or_pipe, pty: false)
-    command = command_or_pipe.join(' | ')
-    output = ''
-    status = nil
+  class << self
+    def call(*command_or_pipe, pty: false)
+      command = command_or_pipe.join(' | ')
+      output, status = pty ? execute_with_pty(command) : execute_with_tty(command)
+      raise "Command failed: #{command}" unless status == 0
 
-    if pty
-      begin
-        PTY.spawn(command) do |stdout, stdin, pid|
-          begin
-            stdout.each_char { |c| print c; output << c }
-          rescue Errno::EIO
-            # End of output
-          end
-          Process.wait(pid)
-          status = $?.exitstatus
-        end
-      rescue PTY::ChildExited
-        # Child process exited
-      end
-    else
+      output
+    end
+
+    private
+
+    def execute_with_tty(command)
+      output = ''
       Open3.popen2e(command) do |stdin, stdout_err, wait_thr|
         while line = stdout_err.gets
           puts line
@@ -29,11 +22,25 @@ module SystemHelper
         end
 
         status = wait_thr.value
+        raise "Command failed: #{command}" unless status == 0
       end
+
+      [output, status]
     end
 
-    raise "Command failed: #{command}" unless status == 0
+    def execute_with_pty(command)
+      output = ''
 
-    output
+      PTY.spawn(command) do |stdout, stdin, pid|
+        begin
+          stdout.each_char { |c| print c; output << c }
+        rescue Errno::EIO
+          # End of output
+        end
+        Process.wait(pid)
+      end
+
+      [output, $?.exitstatus]
+    end
   end
 end
