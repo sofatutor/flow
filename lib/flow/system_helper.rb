a3bdf4f5d66c61530ebbb6a0f18935ccbd1b5b23
Manuel Fittko <manuel.fittko@sofatutor.com>
2024-12-13 19:58:24 +0100

    refactor: update SystemHelper to use system call for command execution

--- lib/flow/system_helper.rb
+++ lib/flow/system_helper.rb
@@ -3,8 +3,18 @@ require 'open3'
 module SystemHelper
   def self.call(*command_or_pipe)
     command = command_or_pipe.join(' | ')
-    output = `#{command}`
-    raise "Command failed: #{command}" unless $?.success?
+    output = ''
+    error = ''
+    status = nil
+
+    Open3.popen3({ 'TERM' => 'xterm-256color' }, command) do |stdin, stdout, stderr, wait_thr|
+      stdin.close
+      output = stdout.read
+      error = stderr.read
+      status = wait_thr.value
+    end
+
+    raise "Command failed: #{error}" unless status.success?
 
     output
   end
