efa3921f9f764f338c531eef8219e1c7fe85067f
Steffen Schröder <steffen.schroeder@sofatutor.com>
2025-02-07 14:41:52 +0100

    refactor to get branch names from pull request

--- .github/actions/gem-dependency-updater/scripts/gem_dependency_updater.rb
+++ .github/actions/gem-dependency-updater/scripts/gem_dependency_updater.rb
@@ -3,27 +3,19 @@
 require 'json'
 
 class GemDependencyUpdater
-  USAGE_MESSAGE = "Usage: #{$0} <branch_name> <gem_name>"
+  USAGE_MESSAGE = "Usage: #{$0} <gem_name>"
   GIT_USER_NAME = 'sofatutor-bot'
   GIT_USER_EMAIL = 'operations+github-bot@sofatutor.com'
   GEMFILE_PATH = 'Gemfile'
   BASE_BRANCH = 'main'
 
-  def initialize(branch_name:, gem_name:)
+  def initialize(gem_name:)
     @github_event = JSON.parse(ENV['GITHUB_EVENT'])
 
-    @branch_name = branch_name
     @gem_name = gem_name
 
-    @dependent_repo_branch_name =
-      if is_merge_event?
-        @github_event['pull_request']['head']['ref']
-      else
-        @branch_name
-      end
-
     if @github_event['pull_request']['labels'].any? { |label| label['name'] == 'avoid gem dependency updater' }
-      puts "Skipping gem dependency update for branch '#{@branch_name}' due to 'avoid gem dependency updater' label."
+      puts "Skipping gem dependency update for branch '#{branch_name}' due to 'avoid gem dependency updater' label."
       exit
     end
 
@@ -39,33 +31,42 @@ class GemDependencyUpdater
 
   private
 
-  def is_merge_event?
-    return false unless @github_event['action'] == 'closed'
-    return false unless @github_event['pull_request']
+  def branch_name
+    if merged?
+      @github_event['pull_request']['base']['ref']
+    else
+      @github_event['pull_request']['head']['ref']
+    end
+  end
 
+  def dependent_repo_branch_name
+    @github_event['pull_request']['head']['ref']
+  end
+
+  def merged?
     @github_event['pull_request']['merged'] == true
   end
 
   def validate_arguments
-    abort(USAGE_MESSAGE) if @branch_name.nil? || @branch_name.strip.empty? || @gem_name.nil? || @gem_name.strip.empty?
+    abort(USAGE_MESSAGE) if @gem_name.nil? || @gem_name.strip.empty?
   end
 
   def checkout_branch
     execute_command("git fetch --depth=1 origin", "Failed to fetch from origin.")
     checkout_cmd = <<~CMD
-      git checkout #{@dependent_repo_branch_name} 2>/dev/null \
-        || git checkout -b #{@dependent_repo_branch_name} origin/#{@dependent_repo_branch_name} 2>/dev/null \
-        || git checkout -b #{@dependent_repo_branch_name}
+      git checkout #{dependent_repo_branch_name} 2>/dev/null \
+        || git checkout -b #{dependent_repo_branch_name} origin/#{dependent_repo_branch_name} 2>/dev/null \
+        || git checkout -b #{dependent_repo_branch_name}
     CMD
-    execute_command(checkout_cmd, "Failed to checkout or create branch '#{@dependent_repo_branch_name}'.")
-    execute_command("git pull", "Failed to pull latest changes for branch '#{@dependent_repo_branch_name}'.", graceful: true)
+    execute_command(checkout_cmd, "Failed to checkout or create branch '#{dependent_repo_branch_name}'.")
+    execute_command("git pull", "Failed to pull latest changes for branch '#{dependent_repo_branch_name}'.", graceful: true)
   end
 
   def update_gem_dependency
     execute_command("bundle config set --local frozen false", "Failed to set bundle config.")
     gemfile = File.read(GEMFILE_PATH)
     new_gemfile = gemfile.gsub(/(gem '#{Regexp.escape(@gem_name)}',.*branch: )'[^']*'/) do
-      "#{$1}'#{@branch_name}'"
+      "#{$1}'#{branch_name}'"
     end
     File.write(GEMFILE_PATH, new_gemfile)
     execute_command("bundle update #{@gem_name} --conservative", "Failed to update gem dependency.")
@@ -74,7 +75,7 @@ class GemDependencyUpdater
   def commit_and_push_changes
     configure_git_user
     execute_command('git add Gemfile Gemfile.lock')
-    commit_message = "Update #{@gem_name} to feature branch #{@branch_name}"
+    commit_message = "Update #{@gem_name} to feature branch #{branch_name}"
     output = execute_command("git commit -m \"#{commit_message}\"", "Failed to commit changes.", graceful: true)
 
     if output.include?('nothing to commit, working tree clean')
@@ -82,24 +83,24 @@ class GemDependencyUpdater
       return
     end
 
-    execute_command("git push origin #{@dependent_repo_branch_name}")
+    execute_command("git push origin #{dependent_repo_branch_name}")
   end
 
   def create_pull_request
     create_pr_command = [
       "gh pr create",
-      "--title \"Update #{@gem_name} to branch #{@branch_name}\"",
+      "--title \"Update #{@gem_name} to branch #{branch_name}\"",
       "--body \"This PR updates the #{@gem_name} to the latest feature branch.\"",
-      "--head #{@dependent_repo_branch_name}",
+      "--head #{dependent_repo_branch_name}",
       "--base #{BASE_BRANCH}"
     ].join(' ')
 
-    puts "Creating pull request for branch '#{@branch_name}'..."
+    puts "Creating pull request for branch '#{branch_name}'..."
 
-    output = execute_command(create_pr_command, "Failed to create pull request.", graceful: true)
+    output = execute_command(create_pr_command, 'Failed to create pull request.', graceful: true)
 
     if output.include?('already exists')
-      puts "Pull request already exists for branch '#{@branch_name}'."
+      puts "Pull request already exists for branch '#{branch_name}'."
       return
     end
   end
@@ -127,4 +128,4 @@ class GemDependencyUpdater
   end
 end
 
-GemDependencyUpdater.new(branch_name: ARGV[0], gem_name: ARGV[1]).call
+GemDependencyUpdater.new(gem_name: ARGV[0]).call
