a2192984ab603290743681c8eaf2195b566f6d4d
Steffen Schröder <steffen.schroeder@sofatutor.com>
2025-03-05 15:15:54 +0100

    Implement gem change checker in ruby

---
+++ .github/actions/gem-change-checker/scripts/gem_change_checker.rb
@@ -0,0 +1,78 @@
+#!/usr/bin/env ruby
+
+require 'json'
+
+class GemChangeChecker
+  USAGE_MESSAGE = "Usage: #{$0} <gem_name>"
+  BASE_BRANCH = 'main'
+
+  def initialize(gem_name:)
+    @github_event = JSON.parse(ENV['GITHUB_EVENT'])
+
+    @gem_name = gem_name
+
+    validate_arguments
+  end
+
+  def call
+    if current_body.contains?("[#{@gem_name} PR](")
+      puts "PR already contains a link to the #{@gem_name} changes."
+      return
+    end
+
+    if old_revision.empty? || new_revision.empty? || old_revision == new_revision
+      puts "No relevant changes detected."
+      return
+    end
+
+    updated_body = case current_body
+                   when /#{link_marker}/
+                     current_body.gsub(/#{link_marker}\(.*\)/, new_link)
+                   else
+                     "#{new_link}\n\n#{current_body}"
+                   end
+
+    `gh pr edit #{pr_number} --body "#{updated_body}"`
+  end
+
+  private
+
+  def validate_arguments
+    abort(USAGE_MESSAGE) if @gem_name.nil? || @gem_name.strip.empty?
+  end
+
+  def gem_repo_url
+    "https://github.com/sofatutor/#{@gem_name}"
+  end
+
+  def old_revision
+    @old_revision ||= `git show origin/#{BASE_BRANCH}:Gemfile.lock | grep -A 1 "#{@gem_name}" | grep revision | awk '{print $2}'`
+  end
+
+  def new_revision
+    @new_revision ||= `grep -A 1 "#{@gem_name}" Gemfile.lock | grep revision | awk '{print $2}'`
+  end
+
+  def compare_url
+    "#{gem_repo_url}/compare/#{old_revision}...#{new_revision}"
+  end
+
+  def pr_number
+    @github_event['pull_request']['number']
+  end
+
+  def current_body
+    @current_body ||= `gh pr view #{pr_number} --json body -q '.body'`
+    @current_body.strip
+  end
+
+  def link_marker
+    "[#{@gem_name} Changes]"
+  end
+
+  def new_link
+    "#{link_marker}(#{compare_url})"
+  end
+end
+
+GemDependencyUpdater.new(gem_name: ARGV[0]).call
